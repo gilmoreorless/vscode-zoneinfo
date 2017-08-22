92882e85f20e6ece097509935b44ad37ef151bba
Gilmore Davidson <gilmoreorless@gmail.com>
2017-08-23 08:18:53 +1000

    Moved symbol parsing into a separate file

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,9 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import { parseDocument } from './symbols';
 
-const ZONEINFO_MODE: vscode.DocumentSelector = 'zoneinfo';
+const ZONEINFO_MODE = 'zoneinfo';
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.languages.registerDocumentSymbolProvider(
@@ -13,81 +14,23 @@ export function activate(context: vscode.ExtensionContext) {
 
 export function deactivate() {}
 
-
-const rValidLine = /^(Zone|Rule|Link)/;
-const rWhitespace = /(\s+)/;
-
-function sumLengths(arr: string[], beforeIndex: number) {
-  return arr.slice(0, beforeIndex).reduce((sum, str) => sum + str.length, 0);
-}
-
-type parsedSymbol = {
-  type: string,
-  name: string,
-  parent?: string,
-  index: number,
-}
-
 class ZoneinfoDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
-
-  public parseLine(line: string): parsedSymbol {
-    if (!rValidLine.test(line)) {
-      return null;
-    }
-    const parts = line.split(rWhitespace);
-    const type = parts[0];
-    if (type === 'Zone' || type === 'Rule') {
-      return {
-        type,
-        name: parts[2],
-        index: sumLengths(parts, 2)
-      };
-    }
-    if (type === 'Link') {
-      return {
-        type,
-        name: parts[4],
-        parent: `Link(${parts[2]})`,
-        index: sumLengths(parts, 4)
-      }
-    }
-    return null;
-  }
 
   public provideDocumentSymbols(
     document: vscode.TextDocument, token: vscode.CancellationToken
   ): vscode.ProviderResult<vscode.SymbolInformation[]> {
-    const lineCount = document.lineCount;
-    let symbols = [];
-    let used = new Set();
-    for (let i = 0; i < lineCount; i++) {
-      let line = document.lineAt(i);
-      // Skip comments and empty lines
-      if (line.isEmptyOrWhitespace || line.text.indexOf('#') === 0) {
-        continue;
-      }
 
-      let match = this.parseLine(line.text);
-      if (match) {
-        // Don't add duplicate Rule definitions
-        let key = [match.type, match.name].join(':');
+    let used = new Set();
+    return parseDocument(document)
+      .filter(symbol => {
+        let key = [symbol.type, symbol.name].join(':');
         if (used.has(key)) {
-          continue;
+          return false;
         }
         used.add(key);
-
-        // Find the symbol's position
-        let range = new vscode.Range(
-          i, match.index,
-          i, match.index + match.name.length
-        );
-        symbols.push(new vscode.SymbolInformation(
-          match.name, vscode.SymbolKind.Field, match.parent || match.type,
-          new vscode.Location(document.uri, range)
-        ));
-      }
-    }
-
-    return symbols;
+        return true;
+      })
+      .map(symbol => symbol.toSymbolInformation());
   }
+
 }
---
+++ src/symbols.ts
@@ -0,0 +1,75 @@
+'use strict';
+
+import * as vscode from 'vscode';
+
+type ZoneSymbolType = 'Zone' | 'Rule' | 'Link';
+export class ZoneSymbol {
+  type: ZoneSymbolType;
+  name: string;
+  parent?: string;
+  location: vscode.Location;
+
+  public constructor(
+    type: ZoneSymbolType, name: string, document: vscode.TextDocument, line: number, col: number
+  ) {
+    this.type = type;
+    this.name = name;
+    this.location = this.locationFromLineCol(name, document, line, col);
+  }
+
+  public toSymbolInformation(): vscode.SymbolInformation {
+    return new vscode.SymbolInformation(
+      this.name, vscode.SymbolKind.Field, this.parent || this.type, this.location
+    );
+  }
+
+  private locationFromLineCol(
+    name: string, document: vscode.TextDocument, line: number, col: number
+  ): vscode.Location {
+    let range = new vscode.Range(
+      line, col,
+      line, col + name.length
+    );
+    return new vscode.Location(document.uri, range);
+  }
+}
+
+const rValidLine = /^(Zone|Rule|Link)/;
+const rWhitespaceCapture = /(\s+)/;
+const rWhitespaceOnly = /^\s+$/; // TODO: Use this when parsing line "tokens", some of which are whitespace-only
+
+function sumLengths(arr: string[], beforeIndex: number) {
+  return arr.slice(0, beforeIndex).reduce((sum, str) => sum + str.length, 0);
+}
+
+function parseLine(document: vscode.TextDocument, lineNumber: number): ZoneSymbol {
+  let line = document.lineAt(lineNumber);
+  let text = line.text;
+  // Skip non-definition lines
+  if (line.isEmptyOrWhitespace || line.text.indexOf('#') === 0 || !rValidLine.test(text)) {
+    return null;
+  }
+  const parts = text.split(rWhitespaceCapture);
+  const type = parts[0];
+  if (type === 'Zone' || type === 'Rule') {
+    return new ZoneSymbol(type, parts[2], document, lineNumber, sumLengths(parts, 2));
+  }
+  if (type === 'Link') {
+    let symbol = new ZoneSymbol(type, parts[4], document, lineNumber, sumLengths(parts, 4));
+    symbol.parent = `Link(${parts[2]})`;
+    return symbol;
+  }
+  return null;
+}
+
+export function parseDocument(document: vscode.TextDocument): ZoneSymbol[] {
+  const lineCount = document.lineCount;
+  let symbols = [];
+  for (let i = 0; i < lineCount; i++) {
+    let symbol = parseLine(document, i);
+    if (symbol) {
+      symbols.push(symbol);
+    }
+  }
+  return symbols;
+}
